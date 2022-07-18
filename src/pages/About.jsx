c6d75860e3139bd27040cfa5c72254094dfdfc82
Ronald <ronaldlambertnicolastugadi@gmail.com>
2022-07-19 01:55:07 +0800

    Third commit

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ import Footer from "../components/Footer";
 import Navbar from "../components/Navbar"
 import { mobile } from "../responsive";
 
-const Image = styled.img``;
+const Image = styled.img``
 const Wrapper = styled.div`
 ${mobile({padding: "10px 0px"})}
 //background-image: url("https://images.unsplash.com/photo-1614252235316-8c857d38b5f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1330&q=80");
