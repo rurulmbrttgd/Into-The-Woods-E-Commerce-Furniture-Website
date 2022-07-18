0d8234b76c1f53ba83852ea7b47a2fa12d899ee5
Ronald <ronaldlambertnicolastugadi@gmail.com>
2022-07-18 13:01:05 +0800

    added api link

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -19,8 +19,8 @@ const Products = ({ cat, filters, sort }) => {
       try{
         const res = await axios.get(
           cat
-          ? `http://localhost:5000/api/products?category=${cat}`
-          : "http://localhost:5000/api/products"
+          ? `https://intothewoods.herokuapp.com/api/products?category=${cat}`
+          : "https://intothewoods.herokuapp.com/api/products"
         );
         setProducts(res.data);
       } catch(err) {}
--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = "https://intothewoods.herokuapp.com/api/";
 const persistRoot = JSON.parse(localStorage.getItem("persist:root"))
 const TOKEN = persistRoot
   ? JSON.parse(persistRoot?.user)?.currentUser?.accessToken
